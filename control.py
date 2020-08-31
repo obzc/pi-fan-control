c5794547f3892010d119663cd47aa68a910c3e31
obzc <oktaybzc@gmail.com>
2020-08-31 22:55:10 +0300

    optimized

--- control.py
+++ control.py
@@ -1,24 +1,34 @@
 import threading
 import os
 import RPi.GPIO as GPIO
+from colorama import Fore
+
 PIN = 4
-SINIR = 59
+MAX_TEMP = 61
+MIN_TEMP = 55
+FAN_STATUS = "OFF"
+TIMER = 10.0
 
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN,GPIO.OUT)
 
+
 def printit():
-	threading.Timer(5.0, printit).start()
-	temp = float(os.popen("vcgencmd measure_temp").readline().split('=')[1].split('\'')[0]);
-	if temp > SINIR:
-		print("{} fan çalıştı.".format(temp))
+	global FAN_STATUS
+	threading.Timer(TIMER, printit).start()
+	temp = float(os.popen("vcgencmd measure_temp").readline().split('=')[1].split('\'')[0])
+	if temp > MAX_TEMP:
 		GPIO.output(PIN,GPIO.HIGH)
-	else:
-		print("{} fan durdu".format(temp))
+		FAN_STATUS = "ON"
+	elif (temp < MIN_TEMP):
 		GPIO.output(PIN,GPIO.LOW)
-		
+		FAN_STATUS = "OFF"
+
+	print("Sıcaklık {} derece. Fan durumu: {}".format(temp, FAN_STATUS))
+
+
 printit()
 
 
